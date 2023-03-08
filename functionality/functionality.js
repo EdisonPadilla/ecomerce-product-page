5daa6f4aad2bb599ed3f91d52df3fe0cdc92ca01
EdisonPadilla <geovanny.esfot@outlook.com>
2023-03-08 10:28:58 -0500

    show cart-information-box

--- functionality/functionality.js
+++ functionality/functionality.js
@@ -6,15 +6,23 @@ const menu_icon = d.querySelector(".menu-icon");
 const close_icon = d.querySelector(".close-icon");
 const menu_box = d.querySelector(".nav-menu-box");
 const menu = d.querySelector(".nav-menu");
+const cart = d.querySelector(".cart-icon");
+const cart_information = d.querySelector(".cart-information-box");
 
 console.info(menu_button);
 console.info(menu_icon);
 console.info(close_icon);
 console.info(menu_box);
 console.info(menu);
+console.info(cart);
+console.info(cart_information);
 
 menu_button.addEventListener("click", () => {
   menu_box.classList.toggle("clicked");
   menu_icon.classList.toggle("clicked");
   close_icon.classList.toggle("clicked");
+});
+
+cart.addEventListener("click", () => {
+  cart_information.classList.toggle("show");
 });
