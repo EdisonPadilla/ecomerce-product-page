6301b29d83a17b5a7d3d6a2392caeb5e72d5f7ef
EdisonPadilla <geovanny.esfot@outlook.com>
2023-03-13 16:55:28 -0500

    slider functionality in mobile layout

--- functionality/functionality.js
+++ functionality/functionality.js
@@ -8,6 +8,13 @@ const menu_box = d.querySelector(".nav-menu-box");
 const menu = d.querySelector(".nav-menu");
 const cart = d.querySelector(".cart-icon");
 const cart_information = d.querySelector(".cart-information-box");
+const product_image_box = d.querySelector(".product-image-box");
+const previous_button = d.querySelector(".previous-btn");
+const next_button = d.querySelector(".next-btn");
+let windowWidth = d.documentElement.clientWidth;
+let productBoxWidth = product_image_box.scrollWidth;
+let offsetValue = 0;
+let displacementLimitValue = productBoxWidth - windowWidth;
 
 console.info(menu_button);
 console.info(menu_icon);
@@ -16,6 +23,13 @@ console.info(menu_box);
 console.info(menu);
 console.info(cart);
 console.info(cart_information);
+console.info(product_image_box);
+console.warn(`El ancho del dispositivo es: ${windowWidth}`);
+console.warn(
+  "El ancho de la caja de imagenes del producto es: ",
+  productBoxWidth
+);
+// console.warn("El límite de desplazamiento es: ", displacementLimitValue);
 
 menu_button.addEventListener("click", () => {
   menu_box.classList.toggle("clicked");
@@ -25,4 +39,30 @@ menu_button.addEventListener("click", () => {
 
 cart.addEventListener("click", () => {
   cart_information.classList.toggle("show");
+});
+
+next_button.addEventListener("click", () => {
+  offsetValue += windowWidth;
+  if (offsetValue <= displacementLimitValue) {
+    product_image_box.style.transform = `translateX(-${offsetValue}px)`;
+  } else {
+    offsetValue = displacementLimitValue;
+    console.info("ya pasaron todas las IMAGENES");
+    console.info("valor de desplazamiento: ", offsetValue);
+    return;
+  }
+  console.warn("BOTON NEXT, valor desplazamiento: ", offsetValue);
+});
+
+previous_button.addEventListener("click", () => {
+  offsetValue -= windowWidth;
+  if (offsetValue >= 0) {
+    product_image_box.style.transform = `translateX(-${offsetValue}px)`;
+  } else {
+    offsetValue = 0;
+    console.info("Ya volvió a la primera IMAGEN");
+    console.info("valor de desplazamiento: ", offsetValue);
+    return;
+  }
+  console.warn("BOTON PREVIOUS, valor desplazamiento: ", offsetValue);
 });
